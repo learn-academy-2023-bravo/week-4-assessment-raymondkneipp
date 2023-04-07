9b71f0cde452f3eae1b92294746a423c06269444
raymondkneipp <raymondkneipp@gmail.com>
2023-04-07 16:17:46 -0700

    finish i hope

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,19 +15,47 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+const colors1 = ['purple', 'blue', 'green', 'yellow', 'pink']
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 const colors2 = [
-  "chartreuse",
-  "indigo",
-  "periwinkle",
-  "ochre",
-  "aquamarine",
-  "saffron"
+  'chartreuse',
+  'indigo',
+  'periwinkle',
+  'ochre',
+  'aquamarine',
+  'saffron',
 ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+describe('removeAndShuffle', () => {
+  it('takes in an array, removes the first item from the array and shuffles the remaining content', () => {
+    expect(removeAndShuffle(colors1)).toEqual(
+      expect.arrayContaining(['yellow', 'blue', 'pink', 'green'])
+    )
+    expect(removeAndShuffle(colors2)).toEqual(
+      expect.arrayContaining([
+        'saffron',
+        'aquamarine',
+        'periwinkle',
+        'indigo',
+        'ochre',
+      ])
+    )
+  })
+})
+
 // b) Create the function that makes the test pass.
+
+// Begin the removeAndShuffle function, which takes an array as input.
+// Use the shift() method on the array to remove its first element.
+// Use the sort() method on the array to shuffle its remaining elements. The sort() method takes a callback function that returns a random number between -1 and 1. If the returned number is less than 0, sort the elements in ascending order (-1). Otherwise, sort the elements in descending order (1).
+// Return the shuffled array.
+
+const removeAndShuffle = (array) => {
+  array.shift()
+
+  return array.sort(() => (Math.random() < 0.5 ? -1 : 1))
+}
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
@@ -38,14 +66,55 @@ const votes1 = { upVotes: 13, downVotes: 2 }
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+describe('tally', () => {
+  it('return end tally', () => {
+    expect(tally(votes1)).toEqual(11)
+    expect(tally(votes2)).toEqual(-31)
+  })
+})
+
 // b) Create the function that makes the test pass.
+
+// Begin the tally function, which takes an object as input.
+// Access the upVotes and downVotes properties of the object.
+// Subtract the value of downVotes from the value of upVotes.
+// Return the result of the subtraction as the tally.
+
+const tally = (obj) => {
+  return obj.upVotes - obj.downVotes
+}
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+const dataArray1 = ['array', 'object', 'number', 'string', 'Boolean']
+const dataArray2 = ['string', 'null', 'Boolean', 'string', 'undefined']
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
+describe('removeDups', () => {
+  it('return end tally', () => {
+    expect(removeDups(dataArray1, dataArray2)).toEqual([
+      'array',
+      'object',
+      'number',
+      'string',
+      'Boolean',
+      'null',
+      'undefined',
+    ])
+  })
+})
+
 // b) Create the function that makes the test pass.
+
+// Begin the removeDups function, which takes any number of arrays as input using the rest parameter syntax.
+// Use the reduce method on the input arrays to flatten them into a single array. Start with an empty array as the initial value of the accumulator.
+// Use the Set constructor and the Array.from method to create a new array with duplicate elements removed.
+// Return the new array as the result of the function.
+
+const removeDups = (...arrays) => {
+  return Array.from(
+    new Set(arrays.reduce((acc, curr) => [...acc, ...curr], []))
+  )
+}
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,7 +10,19 @@ reposts2 = 42
 reposts3 = 221
 # Expected output: '221 is odd'
 
+# create a method called is_odd_or_even
+# method will accept an integer
+# use conditional statement to see if num is even using .even? method
+# if even return num is even
+# if odd return num is odd
 
+def is_odd_or_even num
+  "#{num} is #{num.even? ? 'even' : 'odd'}"
+end
+
+puts is_odd_or_even reposts1
+puts is_odd_or_even reposts2
+puts is_odd_or_even reposts3
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
@@ -22,6 +34,16 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+# create a method that accepts a string
+# return string with delete method that holds a string of vowels both capital and lowercase as an argument
+
+def remove_vowels str
+  str.delete("aeiouAEIOU")
+end
+
+puts remove_vowels beatles_album1
+puts remove_vowels beatles_album2
+puts remove_vowels beatles_album3
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
@@ -32,3 +54,14 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+# create a method that accepts a string
+# use reverse method to see if the string is the same and downcase to fix capitalization
+
+def is_palindrome str
+  "#{str} is #{str.downcase.reverse == str.downcase ? '' : 'not '}a palindrome"
+end
+
+puts is_palindrome palindrome_tester1
+puts is_palindrome palindrome_tester2
+puts is_palindrome palindrome_tester3
